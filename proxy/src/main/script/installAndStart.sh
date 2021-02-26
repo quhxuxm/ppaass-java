0d2e492a58de12b72a05ed427ea8a96c47efe583
quh <quhxuxm_quh@163.com>
2021-02-26 09:30:51 +0800

    init

--- proxy/src/main/script/installAndStart.sh
+++ proxy/src/main/script/installAndStart.sh
@@ -1,21 +1,21 @@
 # wget https://github.com/quhxuxm/ppaass/raw/master/distribute/target/ppaass-1.0-SNAPSHOT.zip
 ps -ef | grep proxy-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
-rm -rf /home/build
-rm -rf /home/sourcecode
+rm -rf /tmp/build
+rm -rf /tmp/sourcecode
 # Build
-mkdir /home/sourcecode
-mkdir /home/build
-cd /home/sourcecode
+mkdir /tmp/sourcecode
+mkdir /tmp/build
+cd /tmp/sourcecode
 git clone https://github.com/quhxuxm/ppaass.git ppaass
-cd /home/sourcecode/ppaass
+cd /tmp/sourcecode/ppaass
 git pull
 mvn clean package
 # ps -ef | grep gradle | grep -v grep | awk '{print $2}' | xargs kill -9
-cp /home/sourcecode/ppaass/distribution/target/ppaass-1.0-SNAPSHOT.zip /home/build
-cd /home/build
+cp /tmp/sourcecode/ppaass/distribution/target/ppaass-1.0-SNAPSHOT.zip /tmp/build
+cd /tmp/build
 chmod 777 ppaass-1.0-SNAPSHOT.zip
 unzip ppaass-1.0-SNAPSHOT.zip
-cd /home/build/proxy
+cd /tmp/build/proxy
 chmod 777 *.sh
-nohup ./start.sh >run.log 2>&1 &
+sudo nohup ./start.sh >run.log 2>&1 &
 ps -ef|grep java|grep proxy-1.0-SNAPSHOT.jar
