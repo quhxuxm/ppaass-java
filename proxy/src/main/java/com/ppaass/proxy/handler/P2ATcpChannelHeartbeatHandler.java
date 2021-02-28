dda7faba80c4091f7d50e8621482800bdc8e57fa
quhxuxm_quh@163.com <quhxuxm_quh@163.com>
2021-02-28 11:35:22 +0800

    init

--- proxy/src/main/java/com/ppaass/proxy/handler/P2ATcpChannelHeartbeatHandler.java
+++ proxy/src/main/java/com/ppaass/proxy/handler/P2ATcpChannelHeartbeatHandler.java
@@ -10,6 +10,8 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
@@ -19,6 +21,7 @@ import java.util.TimeZone;
 @ChannelHandler.Sharable
 public class P2ATcpChannelHeartbeatHandler extends ChannelInboundHandlerAdapter {
     private static final TimeZone UTC_TIME_ZONE = TimeZone.getTimeZone("UTC");
+    private static final Logger logger = LoggerFactory.getLogger(P2ATcpChannelHeartbeatHandler.class);
     private final ProxyConfiguration proxyConfiguration;
 
     public P2ATcpChannelHeartbeatHandler(ProxyConfiguration proxyConfiguration) {
@@ -66,12 +69,21 @@ public class P2ATcpChannelHeartbeatHandler extends ChannelInboundHandlerAdapter 
         proxyChannel.writeAndFlush(heartbeatMessage).addListener((ChannelFutureListener) proxyChannelFuture -> {
             if (proxyChannelFuture.isSuccess()) {
                 tcpConnectionInfo.setHeartBeatFailureTimes(0);
+                logger.debug("Heartbeat success with agent, proxy channel = {}, target channel = {}",
+                        proxyChannel.id().asLongText(), tcpConnectionInfo.getTargetTcpChannel().id().asLongText());
                 return;
             }
             if (tcpConnectionInfo.getHeartBeatFailureTimes() >= proxyConfiguration.getProxyTcpChannelHeartbeatRetry()) {
+                logger.error("Heartbeat fail with agent, close it, time = {}, proxy channel = {}, target channel = {}",
+                        tcpConnectionInfo.getHeartBeatFailureTimes(), proxyChannel.id().asLongText(),
+                        tcpConnectionInfo.getTargetTcpChannel().id().asLongText());
                 proxyChannel.close();
+                tcpConnectionInfo.getTargetTcpChannel().close();
                 return;
             }
+            logger.error("Heartbeat fail with agent, time = {}, proxy channel = {}, target channel = {}",
+                    tcpConnectionInfo.getHeartBeatFailureTimes(), proxyChannel.id().asLongText(),
+                    tcpConnectionInfo.getTargetTcpChannel().id().asLongText());
             tcpConnectionInfo.setHeartBeatFailureTimes(tcpConnectionInfo.getHeartBeatFailureTimes() + 1);
         });
     }
