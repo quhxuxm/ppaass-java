b5de963bf7fbb4c4af39d0cb28e847f2f11c94a8
quh <quhxuxm_quh@163.com>
2021-03-02 11:18:00 +0800

    init

--- common/src/main/java/com/ppaass/common/log/PpaassLogger.java
+++ common/src/main/java/com/ppaass/common/log/PpaassLogger.java
@@ -27,28 +27,28 @@ public class PpaassLogger {
     }
 
     public <T> void debug(Class<T> targetClass, Supplier<String> logSupplier, Supplier<Object[]> argumentsSupplier) {
-        var logger = LoggerFactory.getLogger(targetClass);
+        var logger = loggers.get(targetClass);
         if (logger.isDebugEnabled()) {
             logger.debug(logSupplier.get(), argumentsSupplier.get());
         }
     }
 
     public <T> void warn(Class<T> targetClass, Supplier<String> logSupplier, Supplier<Object[]> argumentsSupplier) {
-        var logger = LoggerFactory.getLogger(targetClass);
+        var logger = loggers.get(targetClass);
         if (logger.isWarnEnabled()) {
             logger.warn(logSupplier.get(), argumentsSupplier.get());
         }
     }
 
     public <T> void error(Class<T> targetClass, Supplier<String> logSupplier, Supplier<Object[]> argumentsSupplier) {
-        var logger = LoggerFactory.getLogger(targetClass);
+        var logger = loggers.get(targetClass);
         if (logger.isErrorEnabled()) {
             logger.error(logSupplier.get(), argumentsSupplier.get());
         }
     }
 
     public <T> void trace(Class<T> targetClass, Supplier<String> logSupplier, Supplier<Object[]> argumentsSupplier) {
-        var logger = LoggerFactory.getLogger(targetClass);
+        var logger = loggers.get(targetClass);
         if (logger.isTraceEnabled()) {
             logger.trace(logSupplier.get(), argumentsSupplier.get());
         }
