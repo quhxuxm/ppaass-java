e921b04eb45751d0b315ac0e19b8abba7a4eea2c
quhxuxm_quh@163.com <quhxuxm_quh@163.com>
2021-04-10 18:51:32 +0800

    init

--- proxy/src/main/script/installAndStart.sh
+++ proxy/src/main/script/installAndStart.sh
@@ -5,7 +5,7 @@ sudo iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 sudo mkdir /opt/java
 sudo apt install maven -y
 sudo apt install unzip -y
-wget https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz
+sudo wget https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz
 sudo tar -zxf openjdk-15.0.2_linux-x64_bin.tar.gz -C /opt/java/
 sudo update-alternatives --install /usr/bin/java java /opt/java/jdk-15.0.2/bin/java 100
 sudo update-alternatives --config java
@@ -18,16 +18,16 @@ sudo rm -rf /tmp/sourcecode
 sudo mkdir /tmp/sourcecode
 sudo mkdir /tmp/build
 cd /tmp/sourcecode
-git clone https://github.com/quhxuxm/ppaass.git ppaass
+sudo git clone https://github.com/quhxuxm/ppaass.git ppaass
 cd /tmp/sourcecode/ppaass
-git pull
-mvn clean package
+sudo git pull
+sudo mvn clean package
 # ps -ef | grep gradle | grep -v grep | awk '{print $2}' | xargs kill -9
-cp /tmp/sourcecode/ppaass/distribution/target/ppaass-1.0-SNAPSHOT.zip /tmp/build
+sudo cp /tmp/sourcecode/ppaass/distribution/target/ppaass-1.0-SNAPSHOT.zip /tmp/build
 cd /tmp/build
-chmod 777 ppaass-1.0-SNAPSHOT.zip
-unzip ppaass-1.0-SNAPSHOT.zip
+sudo chmod 777 ppaass-1.0-SNAPSHOT.zip
+sudo unzip ppaass-1.0-SNAPSHOT.zip
 cd /tmp/build/proxy
-chmod 777 *.sh
+sudo chmod 777 *.sh
 sudo nohup ./start.sh >run.log 2>&1 &
-ps -ef|grep java|grep proxy-1.0-SNAPSHOT.jar
+sudo ps -ef|grep java|grep proxy-1.0-SNAPSHOT.jar
