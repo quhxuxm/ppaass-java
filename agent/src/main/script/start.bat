5b4878e99dca798a04b98cce3be3b6752ccbfa98
quh <quhxuxm_quh@163.com>
2021-09-26 09:15:22 +0800

    optimize

--- agent/src/main/script/start.bat
+++ agent/src/main/script/start.bat
@@ -1,2 +1,2 @@
 set JAVA_HOME=C:\Software\jdk-17
-start %JAVA_HOME%\bin\javaw -server -Dcom.sun.management.jmxremote.port=11181 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=127.0.0.1 -XX:+UseParallelGC -jar agent-1.0-SNAPSHOT.jar --spring.config.location=classpath:application.properties,optional:./application.properties > run.log 2>&1 &
+start %JAVA_HOME%\bin\javaw -server -Dcom.sun.management.jmxremote.port=11181 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=127.0.0.1 -jar agent-1.0-SNAPSHOT.jar --spring.config.location=classpath:application.properties,optional:./application.properties > run.log 2>&1 &
